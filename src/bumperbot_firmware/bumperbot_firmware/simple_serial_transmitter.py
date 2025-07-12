fac3dbdd1f8e488af1f111cce06c4383c33aa8a2
SakifKhan98 <sakifkhan98@gmail.com>
2025-07-11 20:20:20 -0500

    Added ros2_control and imu functionality

--- src/bumperbot_firmware/bumperbot_firmware/simple_serial_transmitter.py
+++ src/bumperbot_firmware/bumperbot_firmware/simple_serial_transmitter.py
@@ -1,5 +1,4 @@
-#! /usr/bin/env python3
-
+#!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
@@ -7,26 +6,21 @@ import serial
 
 
 class SimpleSerialTransmitter(Node):
-
     def __init__(self):
         super().__init__("simple_serial_transmitter")
-        self.declare_parameter("port", "/dev/ttyS0")
+
+        self.declare_parameter("port", "/dev/ttyUSB0")
         self.declare_parameter("baudrate", 115200)
 
         self.port_ = self.get_parameter("port").value
         self.baudrate_ = self.get_parameter("baudrate").value
 
-        self.arduino_ = serial.Serial(
-            port=self.port_,
-            baudrate=self.baudrate_,
-            timeout=0.1,  # Optional: set a timeout for read operations
-        )
-        self.sub_ = self.create_subscription(
-            String, "serial_transmitter", self.msgCallback, 10
-        )
+        self.sub_ = self.create_subscription(String, "serial_transmitter", self.msgCallback, 10)
         self.sub_
+        self.arduino_ = serial.Serial(port=self.port_, baudrate=self.baudrate_, timeout=0.1)
 
     def msgCallback(self, msg):
+        self.get_logger().info("New message received, publishing on serial: %s" % self.arduino_.name)
         self.arduino_.write(msg.data.encode("utf-8"))
 
 
@@ -35,10 +29,10 @@ def main():
 
     simple_serial_transmitter = SimpleSerialTransmitter()
     rclpy.spin(simple_serial_transmitter)
-
+    
     simple_serial_transmitter.destroy_node()
     rclpy.shutdown()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
